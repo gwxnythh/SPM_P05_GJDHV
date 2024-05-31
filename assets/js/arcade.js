da23a7846034b5cd4c827961918b811a91f75897
gwxnythh <S10262576@connect.np.edu.sg>
2024-05-31 22:44:11 +0800

    Temporary Disabled Element

--- assets/js/arcade.js
+++ assets/js/arcade.js
@@ -1,3 +1,20 @@
+// No Inspect Element (delete before submission)
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    }
+    
+
 // Get the restart button
 const restartButton = document.getElementById("restart-button");
 
